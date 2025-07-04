1205a6f4d91d4c6f7c54e56df4ae5cb74ec23257
Nakrin <nakarin1730@gmail.com>
2025-07-04 20:25:11 +0700

    ex-1and2.js

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,13 @@
-//Exercise #1: Variable Creation
-//Start coding here
+Exercise #1: Variable Creation
+let firstName = "Karter";
+let lastName = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
 
-//Exercise #2: Variable Reassignment
-//Start coding here
+const restaurantName = "Chef's Table by Chef James";
+const coursePrice = 15000;
+const numberOfRestaurants = 1500;
+
+Exercise #2: Variable Reassignment
+address = "(970) 878-53292400 County Rd #4 Meeker, Colorado(CO), 81641";
+coursePrice = 5000;
+numberOfRestaurants = 2500;
